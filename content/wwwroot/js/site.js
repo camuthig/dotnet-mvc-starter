7d09bb7034d022d37b639eb302b21ae0a1e21227
Chris Muthig <camuthig@gmail.com>
2018-11-17 00:11:56 -0800

    Use LibMan to add Bulma for styling

--- content/wwwroot/js/site.js
+++ content/wwwroot/js/site.js
@@ -1,4 +1,13 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// The following code is based off a toggle menu by @Bradcomp
+// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
+(function() {
+  var burger = document.querySelector('.burger');
+  var menu = document.querySelector('#'+burger.dataset.target);
+  burger.addEventListener('click', function() {
+      burger.classList.toggle('is-active');
+      menu.classList.toggle('is-active');
+  });
+})();
